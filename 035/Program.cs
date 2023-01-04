a90f7675cad217859f0e492724346c766054e36f
Kseniia <ks.kasoian@gmail.com>
2023-01-05 02:01:33 +0300

    Homework 4 finished

--- 035/Program.cs
+++ 035/Program.cs
@@ -1,2 +1,36 @@
 ﻿//Написать программу замены элементов массива на противоположные
 
+int[] RandomArray(int size=10,int min=1,int max=10)
+{
+     int[] a=new int [size];
+     Random random=new Random();
+     for(int i=0;i<size;i++)
+          a[i]=random.Next(min,max+1);
+     return a;
+}
+
+int[] ChangeNumbersInArray(int[] a)
+{
+     for(int i=0; i<a.Length/2; i++)
+          {
+             int temp=a[i];
+             a[i]=a[^(1+i)];
+             a[^(1+i)]=temp;
+          }
+     return a;    
+}
+
+void PrintArray(int[] array)
+{
+for(int i=0; i<array.Length;i++) System.Console.Write($"{array[i]} ");
+}
+
+System.Console.WriteLine("Укажите количество элементов будущего массива:");
+int N=Convert.ToInt32(Console.ReadLine());
+int[] array=RandomArray(N,1,10);
+PrintArray(array);
+
+System.Console.WriteLine();
+
+int[] newArray=ChangeNumbersInArray(array);
+PrintArray(newArray);
--- 040/Program.cs
+++ 040/Program.cs
@@ -2,3 +2,32 @@
 Создайте массив, который является произведением пар чисел в одномерном массиве a. 
 Парой считаем первый и последний элемент, второй и предпоследний и т.д.*/
 
+int[] RandomArray(int size=10,int min=1,int max=10)
+{
+     int[] a=new int [size];
+     Random random=new Random();
+     for(int i=0;i<size;i++)
+          a[i]=random.Next(min,max+1);
+     return a;
+}
+
+int[] MultiplyPairArray(int[] a)
+{
+     int size=a.Length;
+     int[] NewArray=new int[size];
+     for(int i=0; i<size; i++) NewArray[i]=a[i]*a[a.Length-1-i];
+return NewArray;    
+}
+
+void PrintArray(int[] array)
+{
+for(int i=0; i<array.Length;i++) System.Console.Write($"{array[i]} ");
+}
+
+int N=Convert.ToInt32(Console.ReadLine());
+int[] array=RandomArray(N,1,10);
+int[] newArray=MultiplyPairArray(array);
+
+PrintArray(array);
+System.Console.WriteLine();
+PrintArray(newArray);
